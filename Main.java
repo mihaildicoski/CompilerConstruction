c1833bd04490fd4068f281bf3fe4b1f0ea122e06
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-09-04 18:07:37 +0200

    BufferedReader added.

--- Main.java
+++ Main.java
@@ -1,26 +1,37 @@
 import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) {
-        
+        try {
+            String code = ""; 
+            String filepath = "input.txt"; 
+            StringBuilder sb = new StringBuilder(); 
 
-        // String code = "main num V_x, text V_y, num V_z begin if eq(V_x, 10) then print V_x else print V_y end";
-        //found a bug that lexer does not work as soon as i put a < symbol in the input code
-        String code = "main num V_x, text V_y, num V_z begin V_x < input if eq(V_x, 10) then print V_x else print \"Value\" end F_addthree(V_x, V_y, V_z) end void F_addthree(num V_a, num V_b, num V_c) begin V_c = add(V_a, 3) print V_c end"; 
-        Lexer lexer = new Lexer(code);
-        List<Token> tokens = lexer.lex();
+            try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+                String line; 
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);  
+                }
+                code = sb.toString(); 
+            } 
 
-        System.out.println("The string input was: ");
-        System.out.println(code);
-        for (Token token : tokens) {
-            System.out.println(token);
+           
+            Lexer lexer = new Lexer(code);
+            List<Token> tokens = lexer.lex();
+
+            System.out.println("The string input was: ");
+            System.out.println(code);
+            for (Token token : tokens) {
+                System.out.println(token);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading the input file: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("There was an error: " + e.getMessage());
         }
-
-
-
-
-
-
-
     }
 }
