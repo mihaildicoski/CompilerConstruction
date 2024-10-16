c722233c2e50e8239d79db83edc235ae91d11b48
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-10-16 15:47:50 +0200

    Refactor for AST conversion

--- ASTConverter.java
+++ ASTConverter.java
@@ -1,18 +1,13 @@
-import java.util.*; 
+import java.util.*;
 
 public class ASTConverter {
-
-    private int nodeCounter = 0; 
-    private Map<Node, Integer> nodeToIdMap = new HashMap<>();
 
     public String generateSyntaxTreeXml(Node rootNode) {
         StringBuilder xml = new StringBuilder();
         xml.append("<SYNTREE>\n");
 
-        // Generate the root XML
         xml.append(generateRootXml(rootNode, 1));
 
-        // Generate the inner nodes and leaf nodes
         xml.append("<INNERNODES>\n");
         xml.append(generateInnerAndLeafNodesXml(rootNode, 1));
         xml.append("</INNERNODES>\n");
@@ -22,16 +17,15 @@ public class ASTConverter {
     }
 
     private String generateRootXml(Node rootNode, int indentLevel) {
-        int rootUnid = getUnid(rootNode);  // Assign and get the unique ID for the root node
         String indent = getIndent(indentLevel);
 
         StringBuilder xml = new StringBuilder();
         xml.append(indent).append("<ROOT>\n");
-        xml.append(indent).append("  <UNID>").append(rootUnid).append("</UNID>\n");
+        xml.append(indent).append("  <UNID>").append(rootNode.getId()).append("</UNID>\n");
         xml.append(indent).append("  <SYMB>").append(rootNode.getType()).append("</SYMB>\n");
         xml.append(indent).append("  <CHILDREN>\n");
         for (Node child : rootNode.getChildren()) {
-            xml.append(indent).append("    <ID>").append(getUnid(child)).append("</ID>\n");
+            xml.append(indent).append("    <ID>").append(child.getId()).append("</ID>\n");
         }
         xml.append(indent).append("  </CHILDREN>\n");
         xml.append(indent).append("</ROOT>\n");
@@ -44,10 +38,10 @@ public class ASTConverter {
 
         for (Node child : node.getChildren()) {
             if (child.getChildren().isEmpty()) {
-                // It's a leaf node
+                // leaf
                 xml.append(generateLeafNodeXml(child, indentLevel + 1));
             } else {
-                // It's an inner node
+                // inner
                 xml.append(generateInnerNodeXml(child, indentLevel + 1));
                 xml.append(generateInnerAndLeafNodesXml(child, indentLevel + 1));  // Recursively process children
             }
@@ -57,18 +51,16 @@ public class ASTConverter {
     }
 
     private String generateInnerNodeXml(Node node, int indentLevel) {
-        int nodeUnid = getUnid(node);
-        int parentUnid = getParentUnid(node);
         String indent = getIndent(indentLevel);
 
         StringBuilder xml = new StringBuilder();
         xml.append(indent).append("<IN>\n");
-        xml.append(indent).append("  <PARENT>").append(parentUnid).append("</PARENT>\n");
-        xml.append(indent).append("  <UNID>").append(nodeUnid).append("</UNID>\n");
+        xml.append(indent).append("  <PARENT>").append(node.getParentId()).append("</PARENT>\n");
+        xml.append(indent).append("  <UNID>").append(node.getId()).append("</UNID>\n");
         xml.append(indent).append("  <SYMB>").append(node.getType()).append("</SYMB>\n");
         xml.append(indent).append("  <CHILDREN>\n");
         for (Node child : node.getChildren()) {
-            xml.append(indent).append("    <ID>").append(getUnid(child)).append("</ID>\n");
+            xml.append(indent).append("    <ID>").append(child.getId()).append("</ID>\n");
         }
         xml.append(indent).append("  </CHILDREN>\n");
         xml.append(indent).append("</IN>\n");
@@ -77,14 +69,12 @@ public class ASTConverter {
     }
 
     private String generateLeafNodeXml(Node node, int indentLevel) {
-        int nodeUnid = getUnid(node);
-        int parentUnid = getParentUnid(node);
         String indent = getIndent(indentLevel);
 
         StringBuilder xml = new StringBuilder();
         xml.append(indent).append("<LEAF>\n");
-        xml.append(indent).append("  <PARENT>").append(parentUnid).append("</PARENT>\n");
-        xml.append(indent).append("  <UNID>").append(nodeUnid).append("</UNID>\n");
+        xml.append(indent).append("  <PARENT>").append(node.getParentId()).append("</PARENT>\n");
+        xml.append(indent).append("  <UNID>").append(node.getId()).append("</UNID>\n");
         xml.append(indent).append("  <TERMINAL>\n");
         xml.append(indent).append("    <TOKEN>").append(node.getValue()).append("</TOKEN>\n");
         xml.append(indent).append("  </TERMINAL>\n");
@@ -93,26 +83,7 @@ public class ASTConverter {
         return xml.toString();
     }
 
-    private int getUnid(Node node) {
-        if (!nodeToIdMap.containsKey(node)) {
-            nodeToIdMap.put(node, nodeCounter++);  // Assign a new unique ID
-        }
-        return nodeToIdMap.get(node);
-    }
-
-    private int getParentUnid(Node node) {
-        for (Map.Entry<Node, Integer> entry : nodeToIdMap.entrySet()) {
-            Node parentNode = entry.getKey();
-            if (parentNode.getChildren().contains(node)) {
-                return entry.getValue();  
-            }
-        }
-        return -1;  
-    }
-
     private String getIndent(int level) {
-        return "  ".repeat(level);  
+        return "  ".repeat(level);
     }
-
-    
 }
