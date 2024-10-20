2aa9ece71c3be3347b49d450d597b092a0f4a84a
Ech0-01 <heinrichniebuhr10@icloud.com>
2024-10-20 18:21:03 +0200

    starting stuff

---
+++ TypeChecker.java
@@ -0,0 +1,28 @@
+
+//add imports here 
+
+//this class will be the typechecker for the language given to us.
+//language found in the grammar.txt file
+
+//import java.lang.reflect.Type;          //dont need this yet
+
+public class TypeChecker {
+
+     private Node startNode;
+     TypeChecker(Node startNode){
+         this.startNode = startNode;
+     }
+    
+
+     //create a method for each non-terminal in grammar.txt
+     //
+
+
+     //func for PROG
+            //get the cildren 
+
+            //case 1: 
+            //case 2:
+}
+
+
